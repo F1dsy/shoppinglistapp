669e0114ff6655d81acd9d0c3437986625dd8c6e
Alexander Bang <>
2022-08-24 21:30:51 +0200

    localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shoppinglistapp/provider.dart';
+import 'package:shoppinglistapp/theme_provider.dart';
 import 'package:shoppinglistapp/views/home_view.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'theme.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<ThemeMode>(
       future: getThemeMode(),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          return ChangeNotifierProvider(
-            create: (context) => ThemeValueNotifier(snapshot.data!),
-            builder: (context, _) {
-              ThemeMode theme = Provider.of<ThemeValueNotifier>(context).value;
-              return MaterialApp(
-                title: 'Flutter Demo',
-                theme: ThemeData(
-                  primarySwatch: Colors.blue,
-                ),
-                darkTheme: ThemeData.dark(),
-                themeMode: theme,
-                debugShowCheckedModeBanner: false,
-                home: const HomeView(),
-              );
-            },
+          return MultiProvider(
+            providers: [
+              ChangeNotifierProvider(
+                create: (context) => ThemeValueNotifier(snapshot.data!),
+              ),
+              ChangeNotifierProvider(
+                  create: (context) => ShoppingListProvider())
+            ],
+            builder: (context, child) => MaterialApp(
+              title: 'Flutter Demo',
+              localizationsDelegates: AppLocalizations.localizationsDelegates,
+              supportedLocales: AppLocalizations.supportedLocales,
+              theme: lightTheme,
+              darkTheme: darkTheme,
+              themeMode: Provider.of<ThemeValueNotifier>(context).value,
+              debugShowCheckedModeBanner: false,
+              home: HomeView(),
+            ),
           );
         } else {
-          return Container();
+          return const SizedBox();
         }
       },
     );
-  }
-}
-
-Future<ThemeMode> getThemeMode() async {
-  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-  bool? isDarkMode = sharedPreferences.getBool("isDarkMode");
-
-  switch (isDarkMode) {
-    case true:
-      return ThemeMode.dark;
-    case false:
-      return ThemeMode.light;
-    default:
-      return ThemeMode.system;
-  }
-}
-
-class ThemeValueNotifier extends ValueNotifier<ThemeMode> {
-  ThemeValueNotifier(super.value);
-
-  @override
-  set value(value) {
-    SharedPreferences.getInstance().then((sharedPreferences) =>
-        sharedPreferences.setBool("isDarkMode", value == ThemeMode.dark));
-    super.value = value;
   }
 }
