54c1f51b5e7ec0282770c49f557d294f85819f26
Alexander Bang <>
2022-08-31 16:23:10 +0200

    push bottom nav to new file

---
+++ lib/components/bottom_navigation_bar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shoppinglistapp/models/food_item.dart';
+import 'package:shoppinglistapp/providers/provider.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.translate(
+      offset: Offset(0, -1 * MediaQuery.of(context).viewInsets.bottom),
+      child: SizedBox(
+        width: double.infinity,
+        height: 70,
+        child: BottomAppBar(
+          elevation: 0,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 18.0, right: 18.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Flexible(
+                  flex: 4,
+                  child: SizedBox(
+                    height: 45,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        fillColor: Colors.white70,
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Flexible(
+                  child: IconButton(
+                      onPressed: () {}, icon: const Icon(Icons.menu)),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
